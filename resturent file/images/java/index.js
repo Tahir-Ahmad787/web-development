9fb1c32bd049d0747846ccc6e2416c2d211644aa
Tahir Ahmad <tr786788@gmail.com>
2026-06-24 12:24:37 +0500

    now we update the resturent design

---
+++ resturent file/images/java/index.js
@@ -0,0 +1,33 @@
+
+
+
+
+/*let num1= "admin";
+let num2= 123;
+console.log(num1);
+if (num1==admin) {
+    console.log(num2);
+    if (num2==123) {
+        console.log("success full login");
+    } else {
+        console.log("incorect password")
+    }
+    
+} else {
+    console.log("name not corect")
+}*/
+let lightS;
+console.log("enter the light color",lightS);
+switch (lightS) {
+    case "red":
+        console.log("stop");
+        break
+case "yellow":
+    break
+        case "green":
+            console.log("allow");
+
+    default:
+        console.log("invalid light");
+        break
+}
